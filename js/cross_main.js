597715f6205b1684a01c71b8bd2edd34a521c5ff
twugo <hm17031@student.miyazaki-u.ac.jp>
2020-04-18 03:43:56 +0900

    Update: メッセージの送受信を追加

--- js/cross_main.js
+++ js/cross_main.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const target = 'http://twugo.github.io/CrossDocumentMessagingTest';
 
     document.getElementById('btn').addEventListener('click', () => {
-        document.getElementById('iframe').contentWindow.postMessage(
+        document.getElementById('frame').contentWindow.postMessage(
             document.getElementById('message').value, target
         );
+    });
+
+    document.addEventListener('message', (ev) => {
+        if(ev.origin !== target) return;
+        console.log(ev.data);
     });
 });
--- js/cross_other.js
+++ js/cross_other.js
@@ -3,5 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const origin = 'http://localhost';
         if(ev.origin !== origin) return;
         document.getElementById('result').textContent = ev.data;
+
+        // 現在の日付を返送
+        const current = new Data();
+        ev.source.postMessage(current, origin);
     });
 });
